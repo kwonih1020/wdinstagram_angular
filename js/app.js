71467b9f38340e7cf01c858356e4477a8afcdad7
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-05-09 22:19:09 -0400

    part2, it is working but getting  undefined error

--- js/app.js
+++ js/app.js
@@ -1,66 +1,104 @@
 "use strict";
 
 angular
-  .module("wdinsta", [
-    "ui.router"
+  .module("wdinstagram", [
+    "ui.router",
+    "ngResource"
   ])
   .config([
     "$stateProvider",
     RouterFunction
   ])
-  .controller("indexController", [
-    "$stateParams",
+  .factory("InstaFactory", [
+    "$resource",
+    InstaFactoryFunction
+  ])
+  .controller("wdiIndexCtrl", [
+    "InstaFactory",
     wdinstagramIndexController
   ])
-  .controller("newController", [
-    "$stateParams",
+  .controller("wdiNewCtrl", [
+    "InstaFactory",
+    "$state",
     wdinstagramNewController
   ])
-  .controller("showController", [
+  .controller("wdiShowCtrl", [
+    "InstaFactory",
     "$stateParams",
     wdinstagramShowController
+])
+  .controller("wdiEditCtrl", [
+    "InstaFactory",
+    "$stateParams",
+    "$state",
+    wdinstagramEditController
   ])
 
-function RouterFunction($stateProvider){
+function RouterFunction( $stateProvider ){
   $stateProvider
   .state("wdiIndex", {
     url: "/instas",
     templateUrl: "js/ng-views/index.html",
-    controller: "indexController",
-    controllAs: "vm"
+    controller: "wdiIndexCtrl",
+    controllerAs: "vm"
   })
   .state("wdiNew", {
     url: "/instas/new",
     templateUrl: "js/ng-views/new.html",
-    controller: "newController",
-    controllAs: "vm"
+    controller: "wdiNewCtrl",
+    controllerAs: "vm"
   })
   .state("wdiShow", {
     url: "/instas/:id",
     templateUrl: "js/ng-views/show.html",
-    controller: "showController",
-    controllAs: "vm"
+    controller: "wdiShowCtrl",
+    controllerAs: "vm"
+  })
+  .state("wdiEdit", {
+    url: "/instas/:id/edit",
+    templateUrl: "js/ng-views/edit.html",
+    controller: "wdiEditCtrl",
+    controllerAs: "vm"
   })
 }
 
-function wdinstagramIndexController() {
-  this.instas = data
+function InstaFactoryFunction( $resource ){
+  return $resource("http://localhost:3000/entries/:id", {}, {  update: { method: "Put" }
+  })
 }
 
-function wdinstagramNewController($stateParams) {
-  this.create = function() {
-    this.insta.save(function(insta){
+function wdinstagramIndexController( InstaFactory ){
+  this.instas = InstaFactory.query()
+}
+
+function wdinstagramNewController( InstaFactory, $state ){
+  this.insta = new InstaFactory()
+  this.create = function(){
+    this.insta.$save(function(insta) {
       $state.go("wdiShow", {id: insta.id})
     })
   }
 }
 
-function wdinstagramShowController($stateParams){
-  this.insta = data[$stateParams.id]
+function wdinstagramShowController(InstaFactory, $stateParams){
+  this.insta = InstaFactory.get({id: $stateParams.id})
 }
 
-let data = [
-  {author: "Ick", body: "This is first instagram body.", photo_url: "http://www.thephoblographer.com/wp-content/uploads/2016/09/instagram-770x433.jpg", id: 0},
-  {author: "Hyun", body: "This is second instagram body.", photo_url: "http://www.thephoblographer.com/wp-content/uploads/2016/09/instagram-770x433.jpg", id: 1},
-  {author: "Kwon", body: "This is third instagram body.", photo_url: "http://www.thephoblographer.com/wp-content/uploads/2016/09/instagram-770x433.jpg", id: 2}
-]
+function wdinstagramEditController( InstaFactory, $stateParams, $state ){
+  this.insta = InstaFactory.get({id: $stateParams.id})
+  this.update = function(){
+    this.insta.$update({id: $stateParams.id }, function(insta) {
+      $state.go("wdiShow", {id: insta.id})
+    })
+  }
+  this.destroy = function(){
+    this.insta.$delete({id: $stateParams.id}, function(insta) {
+      $state.go("wdiIndex")
+    })
+  }
+}
+// let data = [
+//   {author: "Ick", body: "This is first instagram body.", photo_url: "http://www.thephoblographer.com/wp-content/uploads/2016/09/instagram-770x433.jpg", id: 0},
+//   {author: "Hyun", body: "This is second instagram body.", photo_url: "http://www.thephoblographer.com/wp-content/uploads/2016/09/instagram-770x433.jpg", id: 1},
+//   {author: "Kwon", body: "This is third instagram body.", photo_url: "http://www.thephoblographer.com/wp-content/uploads/2016/09/instagram-770x433.jpg", id: 2}
+// ]
